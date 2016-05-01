e28e12e3531a5e1b20439f776704c39605fe55de
keidra <keidrabaldwin@gmail.com>
2016-04-30 23:06:17 -0700

    adding html, css, js files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+ /*BUTTON COLLAPSE*/
+ $( document ).ready(function(){
+   $(".button-collapse").sideNav();
+ });
+
+
+
+/* SNAP TO SCROLL */
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
