ef3725fb4508b6e65fa3fa315bfea7104d1a0a37
keidra <keidrabaldwin@gmail.com>
2016-05-25 08:23:55 -0700

    made major changes to page functionality, added fullpageJS

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
  /*BUTTON COLLAPSE*/
+ 
  $( document ).ready(function(){
-   $(".button-collapse").sideNav();
+    $('#fullpage').fullpage();
+
+    $(".button-collapse").sideNav();
+
+
+    /* SNAP TO SCROLL */
+    $('a[href^="#"]').on('click', function(event) {
+
+        var target = $( $(this).attr('href') );
+
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
  });
-
-
-
-/* SNAP TO SCROLL */
-$('a[href^="#"]').on('click', function(event) {
-
-    var target = $( $(this).attr('href') );
-
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-
-});
-
-
-/* HIDE NAVBAR*/
-
